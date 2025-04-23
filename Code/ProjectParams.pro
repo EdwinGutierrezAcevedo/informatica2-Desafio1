50023a44a2d3a7c492e7909d953848d2fd7d7923
Aframirez14 <a.ramirez1@udea.edu.co>
2025-04-22 21:54:18 -0500

    Creación de módulos
    Creación de módulos- función enmascaramiento(prototipo)

--- Code/ProjectParams.pro
+++ Code/ProjectParams.pro
@@ -1,3 +1,7 @@
 QT += core gui
 CONFIG += console c++17
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    funciones_definidas.cpp
+
+HEADERS += \
+    funciones.h
---
+++ Code/funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+unsigned char* loadPixels(QString input, int &width, int &height);
+bool exportImage(unsigned char* pixelData, int width,int height, QString archivoSalida);
+unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
+
+void xorChannel(unsigned char* pixelData, const unsigned char* otherData, int totalBytes);//Operacion Xor
+void rotateImageChannels(unsigned char* pixelData, int totalBytes, int rotateAmount, bool toLeft);// Operaacion Rotacion
+unsigned char rotateRight(unsigned char byte, int count);
+unsigned char rotateLeft(unsigned char byte, int count);
+#endif // FUNCIONES_H
